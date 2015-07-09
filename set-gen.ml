835f870e79bd5ae523b708e948b9d3b087abb925
Tim McGilchrist <timmcgil@gmail.com>
2015-07-10 07:37:36 +1000

    Add generative set functor.

---
+++ set-gen.ml
@@ -0,0 +1,48 @@
+module type ORDERED = sig
+  type t
+  val compare : t -> t -> int
+end;;
+
+module type SET = sig
+  type elt
+  type t
+  val empty : t
+  val insert : elt -> t -> t
+  val member : elt -> t -> bool
+end;;
+
+(*
+Note: This is a generative functor.
+ *)
+
+module UnbalancedSet (Element : ORDERED) () : (SET with type elt = Element.t)= struct
+  type elt = Element.t
+  type tree = E | T of tree * elt * tree
+  type t = tree
+
+  let empty = E
+
+  let rec member x = function
+    | E -> false
+    | T(a, y, b) when Element.compare x y < 0 -> member x a
+    | T(a, y, b) when Element.compare x y > 0 -> member x b
+    | _ -> true
+
+  let rec insert x = function
+    | E -> T(E, x, E)
+    | T(a, y, b) when Element.compare x y < 0 -> T(insert x a, y, b)
+    | T(a, y, b) when Element.compare x y > 0 -> T(a, y, insert x b)
+    | T(a, y, b) as s -> s
+end;;
+
+module IntOrdering = struct
+  type t = int
+
+  let compare = Pervasives.compare
+end;;
+
+module S = UnbalancedSet(IntOrdering)();;
+
+let r = S.(insert 1 empty)
+S.(member 1 r)  (* true *)
+S.(member 2 r) (* false *)
